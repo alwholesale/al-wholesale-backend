f0f4349453bbffd12a0a18fd493f1edf9fe876bd
Mueezur Rehman <mueezur@alwholesale.ae>
2026-05-09 19:24:38 +0530

    fix whitenoise error

--- core/settings.py
+++ core/settings.py
@@ -146,6 +146,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+WHITENOISE_MANIFEST_STRICT = False
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
